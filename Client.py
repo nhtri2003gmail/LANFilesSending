ed8faf688e603268e3b1975569f6cdb7d58e95c5
Nguyen Huu Tri <nhtri2003@gmail.com>
2020-04-29 19:44:34 +0700

    Send and Receive 2 way

--- Client.py
+++ Client.py
@@ -1,45 +1,91 @@
 import socket
 import time
 import os
+import sys
 
 HOST = '127.0.0.1'
 PORT = 60001
 SEPARATOR = '<SEPARATOR>'
-while True:
-    filename = input("Path to file (include file name) - e.g: C:\\note.txt\n>")
-    if os.path.isfile(filename):
-        break
-    else:
-        print("Cannot find the specified file!")
-filesize = os.path.getsize(filename)
+
+def GetFileName():
+    while True:
+        filename = input("Path to file (include file name) - e.g: C:\\note.txt\n> ")
+        if os.path.isfile(filename):
+            break
+        else:
+            print("Cannot find the specified file!")
+    filesize = os.path.getsize(filename)
+    return [filename, filesize]
 
 def SendOp(s):
     print("[+] Sending file...")
     f = open(filename, 'rb')
-    t = f.read()
+    t = f.read(4096)
     while t:
         s.send(t)
         t = f.read(4096)
+        if not t:
+            break
     f.close()
     print("[+] Done")
         
-def Send():
+def Send(f, filename, filesize):
+    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+    print("[+] Waiting for server\'s reply...")
+    while True:
+        c = s.recv(1024).decode()
+        if c=='y' or c=='n':
+            break
+    if c=='y':
+        print("[+] Server Accept!")
+        SendOp(s)
+    else:
+        print("[+] Server denied!")
+        time.sleep(1.5)
+def ReceiveOp(s):
+    filename = s.recv(1024).decode()
+    print(filename)
+    print('[+] Receiving data...')
+    with open(filename, 'wb') as f:
+        while True:
+            t = s.recv(4096)
+            try:
+                if t==b'Love':
+                    break
+            except:
+                pass
+            f.write(t)
+    print("[+] Done")
+
+
+def Receive(s):
+    file = s.recv(1024)
+    print(file.decode())
+    while True:
+        m = input("Index number > ")
+        if m=='':
+            continue
+        s.send(m.encode())
+        r = s.recv(1024).decode()
+        if r=='ok':
+            break
+        print(r)
+    ReceiveOp(s)
+
+if __name__=='__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"[+] Connecting to {HOST}:{PORT}")
         s.connect((HOST, PORT))
         print(f"[+] Connected successfully!")
-        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
-        print("[+] Waiting for server\'s reply...")
+        print(s.recv(1024).decode())
         while True:
-            c = s.recv(1024).decode()
-            if c=='y' or c=='n':
+            c = input("> ")
+            if c=='1' or c=='2' or c=='3':
                 break
-        if c=='y':
-            print("[+] Server Accept!")
-            SendOp(s)
-        else:
-            print("[+] Server denied!")
-            time.sleep(1.5)
-
-if __name__=='__main__':
-    Send()
+        s.send(c.encode())
+        if c=='1':
+            filename, filesize = GetFileName()
+            Send(s, filename, filesize)
+        elif c=='2':
+            Receive(s)
+    print("Disconnected")
--- Server.py
+++ Server.py
@@ -13,30 +13,28 @@ PORT = 60001
 LOCAL_IP = socket.gethostbyname(socket.gethostname())
 
 def ReceiveOp(filename, conn):
-    print('Receiving data...')
+    print('[+] Receiving data...')
     f = open(filename, 'wb')
     data = conn.recv(4096)
-    while data:
+    while True:
         f.write(data)
         data = conn.recv(4096)
+        if not data:
+            break
     f.close()
+    print("[+] Done")
 
-def Receive():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(5)
-        conn, addr = s.accept()
-        
+def Receive(conn):
+        print("[+] Waiting for file...")
         info = conn.recv(1024).decode()
         filename, filesize = info.split('<SEPARATOR>')
         print('[+] Connected to ', addr)
-        print("File name: " + filename)
-        print("Size: " + humanize.naturalsize(filesize))
-
         if '\\' in filename:
             t = filename.split('\\')
             filename = str(t[len(t)-1])
-        
+        print("File name: " + filename)
+        print("Size: " + humanize.naturalsize(filesize))
+
         while True:
             i = input("Receive the file? [Y/n] (Default Y) > ")
             if i.lower()=='y' or i=='':
@@ -46,10 +44,79 @@ def Receive():
             elif i.lower()=='n':
                 conn.sendall('n'.encode())
                 break
+def SendOp(filename):
+    print("[+] Sending file...")
+    f = open(filename, 'rb')
+    data = f.read(4096)
+    while True:
+        conn.sendall(data)
+        data = f.read(4096)
+        if not data:
+            break
+    time.sleep(1)
 
+    conn.send("Love".encode())
+    f.close()
+    print("[+] Done")
+
+def Send(conn):
+    listdir = os.listdir('./')
+    strFile = ''
+    files = []
+    k=0
+    print("List:")
+    for i in listdir:
+        if os.path.isfile(i):
+            files.append(i)
+    for i in files:
+        print(str(k) + '. ' + str(i))
+        strFile = strFile + str(k) + '. ' + i + '\n'
+        k+=1
+    conn.send(strFile.encode())
+    print('[+] Waiting for client input...')
+    while True:
+        c = conn.recv(1024).decode()
+        print("-> Client: " + c)
+        try:
+            c = int(c)
+        except:
+            conn.send("Not a number".encode())
+            continue
+        if 0<=c and c<k:
+            conn.send('ok'.encode())
+            break
+        else:
+            conn.send('Wrong index'.encode())
+    conn.send(files[c].encode())
+    SendOp(files[c])
+        
 
 print("[+] Server ip: " + LOCAL_IP)
-print()
-           
+ 
 if __name__=='__main__':
-    Receive()
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            print()
+            print('[+] Incoming connection: ', addr)
+            conn.send("1. Send Fild\n2. Get File\n3. Exit from server".encode())
+            try:
+                while True:
+                    c = conn.recv(1024).decode()
+                    if c=='1':
+                        print("Option 1: Get a file")
+                        Receive(conn)
+                        break
+                    elif c=='2':
+                        print("Option 2: Send a file")
+                        Send(conn)
+                        break
+                    elif c=='3':
+                        pass
+                        break
+            except:
+                print("Client disconnected")
+            print("[+] Connection closed")
+
